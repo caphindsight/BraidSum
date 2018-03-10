3ed7f03125242d77dfd219bcd3227b97c8bc038c
Cap. Hindsight <hindsight@yandex.ru>
2018-03-10 22:04:50 +1100

    Library 'brade3': Artin groups for three strands.

---
+++ braid3/src/lib.rs
@@ -0,0 +1,104 @@
+#[cfg(test)]
+mod test;
+
+/// Represents a twist (an atom of braiding) on the set of three strands.
+/// `A` stands for forward-slash twisting of the first two strands.
+/// `B` stands for forward-slash twisting of the last two strands.
+/// `Ainv` and `Binv` stand for inversed `A` and `B` respectively.
+#[derive(Eq, Clone, Copy, Debug, PartialEq)]
+pub enum Twist {
+  A, B, Ainv, Binv
+}
+
+/// Represents a braid (a sequence of twist) on the set of three strands.
+#[derive(Clone, Debug, PartialEq)]
+pub struct Braid {
+  twists: Vec<Twist>,
+}
+
+impl Braid {
+  /// Gives the identity in the braid group (braid with no twists).
+  pub fn identity() -> Braid {
+    Braid { twists: Vec::new() }
+  }
+
+  fn ends_with(&self, ending: &Vec<Twist>) -> bool {
+    let n = self.twists.len();
+    let m = ending.len();
+    if n < m {
+      return false;
+    }
+    for i in 0..m {
+      if self.twists[n - i - 1] != ending[m - i - 1] {
+        return false;
+      }
+    }
+    true
+  }
+
+  /// Returns a list of braids (in lexicographic order) which can be obtained
+  /// from `self` by adding a single twist to its end. Excludes resulting braids
+  /// in non-canonical form (i.e. containing reducible sequences like:
+  /// * `A Ainv`, `B Binv`, `Ainv A`, `Binv B`, because they are equal to `1`.
+  /// * `B A B`, `Binv Ainv Binv`, because they can be reduced to `A B A` and
+  ///   `Ainv Binv Ainv` respectively.
+  /// * `Binv A B A`, `A B A Binv`, `B Ainv Binv Ainv`, `Ainv Binv Ainb B`,
+  ///    because they can be reduced to `A B`, `B A`, Ainv Binv`, `Binv Ainv`
+  ///    respectively.
+  pub fn descendants(&self) -> Vec<Braid> {
+    let mut res = Vec::<Braid>::with_capacity(4);
+    let n = self.twists.len();
+    let mut new_twists = Vec::with_capacity(n + 1);
+    for i in &self.twists {
+      new_twists.push(*i);
+    }
+
+    // We can add `A` to the braid unless it ends with:
+    // * `Ainv`, in which case `Ainv A` == `1`.
+    // * `Binv A B`, in which case `Binv A B A` == `Binv B A B` == `A B`.
+    if !self.ends_with(&vec![Twist::Ainv]) &&
+       !self.ends_with(&vec![Twist::Binv, Twist::A, Twist::B]) {
+      new_twists.push(Twist::A);
+      res.push(Braid { twists: new_twists.clone() });
+      new_twists.pop();
+    }
+
+    // We can add `B` to the braid unless it ends with:
+    // * `Binv`, in which case `Binv B` == `1`.
+    // * `B A`, in which case `B A B` == `A B A`, and we allow the 2nd form.
+    // * `Ainv Binv Ainv`, in which case `Ainv Binv Ainv B` == `Binv Ainv`.
+    if !self.ends_with(&vec![Twist::Binv]) &&
+       !self.ends_with(&vec![Twist::B, Twist::A]) &&
+       !self.ends_with(&vec![Twist::Ainv, Twist::Binv, Twist::Ainv]) {
+      new_twists.push(Twist::B);
+      res.push(Braid { twists: new_twists.clone() });
+      new_twists.pop();
+    }
+
+    // We can add `Ainv` to the braid unless it ends with:
+    // * `A`, in which case `A Ainv` == `1`.
+    // * `B Ainv Binv`, in which case `B Ainv Binv Ainv` == `B Binv Ainv Binv`
+    //     == `Ainv Binv`.
+    if !self.ends_with(&vec![Twist::A]) &&
+       !self.ends_with(&vec![Twist::B, Twist::Ainv, Twist::Binv]) {
+      new_twists.push(Twist::Ainv);
+      res.push(Braid { twists: new_twists.clone() });
+      new_twists.pop();
+    }
+
+    // We can add `Binv` to the braid unless it ends with:
+    // * `B`, in which case `B Binv` == `1`.
+    // * `Binv Ainv`, in which case `Binv Ainv Binv` == `Ainv Binv Ainv`,
+    //     and we allow the 2nd form.
+    // * `A B A`, in which case `A B A Binv` == `B A`.
+    if !self.ends_with(&vec![Twist::B]) &&
+       !self.ends_with(&vec![Twist::Binv, Twist::Ainv]) &&
+       !self.ends_with(&vec![Twist::A, Twist::B, Twist::A]) {
+      new_twists.push(Twist::Binv);
+      res.push(Braid { twists: new_twists.clone() });
+      new_twists.pop();
+    }
+
+    res
+  }
+}
---
+++ braid3/src/test.rs
@@ -0,0 +1,72 @@
+use super::Braid;
+
+fn get_braids_of_length(n: u32) -> Vec<Braid> {
+  if n == 0 {
+    return vec![Braid::identity()];
+  }
+  let prev = get_braids_of_length(n - 1);
+  let mut res = Vec::with_capacity(prev.len() * 4);
+  for i in &prev {
+    res.append(&mut i.descendants());
+  }
+  res.shrink_to_fit();
+  res
+}
+
+#[test]
+fn braids_of_length_1() {
+  let mut braids = get_braids_of_length(1);
+  assert_eq!(braids.len(), 4);
+  braids.reverse();
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [A] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [B] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [Ainv] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [Binv] }");
+}
+
+#[test]
+fn braids_of_length_2() {
+  let mut braids = get_braids_of_length(2);
+  assert_eq!(braids.len(), 12);
+  braids.reverse();
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [A, A] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [A, B] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [A, Binv] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [B, A] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [B, B] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [B, Ainv] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [Ainv, B] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [Ainv, Ainv] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [Ainv, Binv] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [Binv, A] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [Binv, Ainv] }");
+  assert_eq!(format!("{:?}", braids.pop().unwrap()),
+             "Braid { twists: [Binv, Binv] }");
+}
+
+#[test]
+fn braids_of_length_3() {
+  let braids = get_braids_of_length(3);
+  assert_eq!(braids.len(), 34);
+}
+
+#[test]
+fn braids_of_length_4() {
+  let braids = get_braids_of_length(4);
+  assert_eq!(braids.len(), 92);
+}
